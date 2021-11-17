388e8534d3962d58eecde20de362df78f551977d
Abdul Rahman <cracyabdul@gmail.com>
2021-11-17 13:22:18 +0530

    login reducer initial state added and ios workspace created

--- Rn_Language_App/app/store/reducers/LoginReducer.js
+++ Rn_Language_App/app/store/reducers/LoginReducer.js
@@ -1,17 +1,16 @@
-
 'use strict';
 
 import Constants from '../../utils/Constants';
 
 let initialState = {
- 
+  isLoading: false,
 };
 const {
   ACTIONS: {SPLASH_SHOW_LOADING},
 } = Constants;
 
 export const loginState = (state = initialState, action) => {
-  const {type, errorMessage} = action;
+  const {type} = action;
   switch (type) {
     case SPLASH_SHOW_LOADING:
       return {...state, isLoading: true};
