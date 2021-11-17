1fed97859ce2cebaab9f045b193dfac52c27a2ae
Abdul Rahman <cracyabdul@gmail.com>
2021-11-17 17:21:04 +0530

    outside route index removed

--- Rn_Language_App/App.js
+++ Rn_Language_App/App.js
@@ -3,7 +3,7 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import {store} from './app/store';
-import RouteNavigator from './app/index';
+import RouteNavigator from './app/routes/index';
 
 /**
  * Gets the store and registered scenes and sets that to provider
